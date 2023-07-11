92ebfff8b1a14d2a7c245e6b8cea29e1c9901c86
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-07-10 21:11:20 -0400

    adjust API order

--- src/services/portfolio-service.js
+++ src/services/portfolio-service.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'https://portfolio-generator-7s61.onrender.com/api/users' ||
-  'http://localhost:4000/api/users';
-
+const API_BASE = process.env.REACT_APP_APIENDPOINT ||
+'https://portfolio-generator-7s61.onrender.com/api/users';
 export const registerUser = async (userData) => {
   try {
     const response = await axios.post(`${API_BASE}/register`, userData);
