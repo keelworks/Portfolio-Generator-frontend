c6d0d9ee81b2b33098e75106ef7b5d0d4e5b3d4b
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-12-12 12:10:16 -0500

    Jiangwei-Add-upload-avatar-and upload-resume-function

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9ln-h42-A_jrpEZ87mtJXKBnKyjh4XTc',
+  authDomain: 'portfolio-generator-394004.firebaseapp.com',
+  projectId: 'portfolio-generator-394004',
+  storageBucket: 'portfolio-generator-394004.appspot.com',
+  messagingSenderId: '538094099728',
+  appId: '1:538094099728:web:af15bcd5901beccc1a9995',
+  measurementId: 'G-7KPNTDVSPE',
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
