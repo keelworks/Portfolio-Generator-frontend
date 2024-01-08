4779ba0cda4fc063bc18d13c82a85cdbe473f00b
dhakalkishor <kishiskool@helpusdefend.com>
2024-01-08 13:25:40 -0800

    deleted extra stuffs
    Deleted all the information we do no need

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -14,24 +14,18 @@ import {useForm} from '@mantine/form';
 import {Text} from '@mantine/core';
 import {useDispatch, useSelector} from 'react-redux';
 import {updateUserThunk} from '../../services/authorize-thunk';
-import {IconBrandLinkedin, IconFileCv} from '@tabler/icons-react';
 import firebase from '../../firebaseConfig';
 import {getStorage, ref, uploadBytes, getDownloadURL} from 'firebase/storage';
 import React, {useState} from 'react';
 
 const WelcomePage = () => {
   const [avatar, setAvatar] = useState(null);
-  const [resume, setResume] = useState(null);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   const user = useSelector((state) => state.currentUser);
   const dispatch = useDispatch();
   const handleAvatarChange = (e) => {
     setAvatar(e);
   };
-  const handleResumeChange = (e) => {
-    setResume(e);
-  };
-
   const handleAvatarClick = () => {
     setIsViewerOpen(true);
   };
@@ -44,26 +38,14 @@ const WelcomePage = () => {
     return getDownloadURL(storageRef);
   };
 
-  const uploadResume = async () => {
-    if (!resume) return user.resumeUrl;
-    const storage = getStorage(firebase);
-    const storageRef = ref(storage, 'resume/' + resume.name);
-    await uploadBytes(storageRef, resume);
-    return getDownloadURL(storageRef);
-  };
-
   const form = useForm({
     initialValues: {
       avatarUrl: user?.avatarUrl || 'https://firebasestorage.googleapis.com/v0/b/portfolio-generator-394004.appspot.com/o/avatars%2Fcxk.jpg?alt=media&token=29c9ba5e-ea2a-4c76-9e15-4ba58ff13c69',
       resumeUrl: user?.resumeUrl || '',
       firstname: user?.firstName || '',
       lastName: user?.lastName || '',
-      email: user?.email || '',
       profession: user?.profession || '',
       bio: user?.bio || '',
-      experience: user?.experience|| '',
-      location: user?.location || '',
-      linkedin: user?.linkedin || '',
     },
     validate: {
       firstname: (value) => value.trim().length < 2,
@@ -89,18 +71,12 @@ const WelcomePage = () => {
   const handleSubmit = async (values) => {
     try {
       const avatarUrl = await uploadAvatar();
-      const resumeUrl = await uploadResume();
       const userData = {
         firstName: values.firstname,
         lastName: values.lastName,
-        email: values.email,
         profession: values.profession,
         bio: values.bio,
-        experience: values.experience,
-        location: values.location,
-        linkedin: values.linkedin,
         avatarUrl: avatarUrl,
-        resumeUrl: resumeUrl,
       };
       // Dispatch an update action - replace with the actual thunk if different
       const action = updateUserThunk({uid: user._id, userData});
@@ -148,19 +124,9 @@ const WelcomePage = () => {
               placeholder=".jpg .Png are acceptable"
               accept="image/*"
               onChange={handleAvatarChange}
-              style={{flex: 1}}
+              style={{flex: 1, width: 50}}
             />
           </Flex>
-          <FileInput
-            icon={<IconFileCv/>}
-            clearable
-            variant="filled"
-            label="Attach your CV"
-            placeholder="only .PDF acceptable"
-            accept=".pdf"
-            onChange={handleResumeChange}
-            style={{flex: 1}}
-          />
         </Flex>
         <Flex style={{marginTop: '1rem', gap: '1rem'}}>
           <TextInput
@@ -180,13 +146,6 @@ const WelcomePage = () => {
           />
         </Flex>
         <Flex style={{marginTop: '1rem', gap: '1rem'}}>
-          <TextInput
-            label="Email"
-            name="email"
-            variant="filled"
-            {...form.getInputProps('email')}
-            style={{flex: 1}}
-          />
           <NativeSelect
             label="Profession"
             name="profession"
@@ -198,40 +157,10 @@ const WelcomePage = () => {
             style={{flex: 1}}
           />
         </Flex>
-
-        <Flex style={{marginTop: '1rem', gap: '1rem'}}>
-          <NativeSelect
-            label="Experience"
-            name="experience"
-            variant="filled"
-            {...form.getInputProps('experience')}
-            data={
-              ['0-1 years', '1-3 years', '3-5 years', '5-10 years', '10+ years']
-            }
-            style={{flex: 1}}
-          />
-
-          <TextInput
-            label="Location"
-            name="location"
-            variant="filled"
-            {...form.getInputProps('location')}
-            style={{flex: 1}}
-          />
-        </Flex>
-        <TextInput
-          label="LinkedIn Profile URL"
-          icon={<IconBrandLinkedin/>}
-          mt="md"
-          name="linkedin"
-          variant="filled"
-          {...form.getInputProps('linkedin')}
-        />
-
         <Textarea
           mt="md"
-          label="bio"
-          placeholder="Your message"
+          label="Practice Philosophy"
+          placeholder="Practice Philosophy"
           maxRows={10}
           minRows={5}
           autosize
